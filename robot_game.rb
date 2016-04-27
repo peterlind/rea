33d29592e4bfa5c5d0389d9beec33424ceedf764
peterlind <peter@lind.be>
2016-04-27 10:36:24 +0200

    add placement function

--- robot_game.rb
+++ robot_game.rb
@@ -1,3 +1,35 @@
 class RobotGame
+  VALID_X = 0..4
+  VALID_Y = 0..4
+  VALID_DIRECTIONS = [
+    'NORTH'.freeze,
+    'SOUTH'.freeze,
+    'EAST'.freeze,
+    'WEST'.freeze,
+  ].freeze
 
+  def initialize
+  end
+
+  def place(x, y, facing = 'NORTH')
+    ensure_on_board(x, y)
+    ensure_valid_direction(facing)
+    @current_x = x
+    @current_y = y
+    @current_facing = facing
+  end
+
+  def report
+    [@current_x, @current_y, @current_facing]
+  end
+
+  private
+  def ensure_valid_direction(direction)
+    raise "Invalid direction (#{direction})" unless VALID_DIRECTIONS.include?(direction)
+  end
+
+  def ensure_on_board(x, y)
+    raise "Invalid x (#{x})" unless VALID_X.include?(x)
+    raise "Invalid y (#{x})" unless VALID_Y.include?(y)
+  end
 end
--- robot_game_spec.rb
+++ robot_game_spec.rb
@@ -1,4 +1,29 @@
-require './robot_game'
+require "./robot_game"
 describe RobotGame do
-
+  let(:game) { RobotGame.new }
+  context "initial placement" do
+    it "accepts placement" do
+      game.place(0, 0, "NORTH")
+      expect(game.report).to eq([0, 0, "NORTH"])
+    end
+    it "ensures placement is within 5x5 grid" do
+      expect { game.place(0, 0) }.to_not raise_exception
+      expect { game.place(4, 4) }.to_not raise_exception
+      expect { game.place(-1, 0) }.to raise_exception(StandardError)
+      expect { game.place(5, 0) }.to raise_exception(StandardError)
+      expect { game.place(0, -2) }.to raise_exception(StandardError)
+      expect { game.place(0, 5) }.to raise_exception(StandardError)
+    end
+    it "ensures valid direction parameter" do
+      %w(NORTH SOUTH EAST WEST).each do |direction|
+        expect { game.place(0, 0, direction) }.to_not raise_exception
+      end
+      expect { game.place(0, 0, "NORTHEAST") }.to raise_exception(StandardError)
+      expect { game.place(0, 0, "GARBLE") }.to raise_exception(StandardError)
+    end
+    it "reports x, y and direction after placement" do
+      game.place(1, 2, 'SOUTH')
+      expect(game.report).to eq([1, 2, 'SOUTH'])
+    end
+  end
 end
