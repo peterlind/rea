a92919443914e7b4c060f29e1b2da39f874e55b3
peterlind <peter@lind.be>
2016-04-27 11:29:56 +0200

    use string constants for direction

--- robot_game_spec.rb
+++ robot_game_spec.rb
@@ -1,11 +1,16 @@
 require "./robot_game"
 describe RobotGame do
+  N = "NORTH".freeze
+  S = "SOUTH".freeze
+  W = "WEST".freeze
+  E = "EAST".freeze
+
   let(:game) { RobotGame.new }
 
   context "initial placement" do
     it "accepts placement" do
-      game.place(0, 0, "NORTH")
-      expect(game.report).to eq([0, 0, "NORTH"])
+      game.place(0, 0, N)
+      expect(game.report).to eq([0, 0, N])
     end
     it "ensures placement is within 5x5 grid" do
       expect { game.place(0, 0) }.to_not raise_exception
@@ -23,8 +28,8 @@ describe RobotGame do
       expect { game.place(0, 0, "GARBLE") }.to raise_exception(StandardError)
     end
     it "reports x, y and direction after placement" do
-      game.place(1, 2, 'SOUTH')
-      expect(game.report).to eq([1, 2, 'SOUTH'])
+      game.place(1, 2, S)
+      expect(game.report).to eq([1, 2, S])
     end
   end
 
@@ -36,72 +41,72 @@ describe RobotGame do
 
   context "moving" do
     it "accepts movement within grid" do
-      game.place(0, 0, 'NORTH')
+      game.place(0, 0, N)
       game.move
-      expect(game.report).to eq([0, 1, 'NORTH'])
+      expect(game.report).to eq([0, 1, N])
 
-      game.place(0, 0, 'EAST')
+      game.place(0, 0, E)
       game.move
-      expect(game.report).to eq([1, 0, 'EAST'])
+      expect(game.report).to eq([1, 0, E])
     end
     it "does nothing is movement would put robot off grid" do
-      game.place(0, 0, 'SOUTH')
+      game.place(0, 0, S)
       game.move
-      expect(game.report).to eq([0, 0, 'SOUTH'])
+      expect(game.report).to eq([0, 0, S])
 
-      game.place(0, 0, 'WEST')
+      game.place(0, 0, W)
       game.move
-      expect(game.report).to eq([0, 0, 'WEST'])
+      expect(game.report).to eq([0, 0, W])
     end
   end
 
   context "turning" do
     it "changes direction after left/right command" do
-      game.place(0, 0, 'NORTH')
+      game.place(0, 0, N)
       game.left
-      expect(game.report).to eq([0, 0, 'WEST'])
+      expect(game.report).to eq([0, 0, W])
 
       game.right
-      expect(game.report).to eq([0, 0, 'NORTH'])
+      expect(game.report).to eq([0, 0, N])
 
       game.right
-      expect(game.report).to eq([0, 0, 'EAST'])
+      expect(game.report).to eq([0, 0, E])
 
       game.left
       game.left
       game.left
-      expect(game.report).to eq([0, 0, 'SOUTH'])
+      expect(game.report).to eq([0, 0, S])
       game.left
-      expect(game.report).to eq([0, 0, 'EAST'])
+      expect(game.report).to eq([0, 0, E])
 
       game.right
-      expect(game.report).to eq([0, 0, 'SOUTH'])
+      expect(game.report).to eq([0, 0, S])
       game.right
-      expect(game.report).to eq([0, 0, 'WEST'])
+      expect(game.report).to eq([0, 0, W])
       game.right
       game.right
-      expect(game.report).to eq([0, 0, 'EAST'])
+      expect(game.report).to eq([0, 0, E])
     end
   end
 
   context "game examples" do
     it "passes example a" do
-      game.place(0, 0, 'NORTH')
+      game.place(0, 0, N)
       game.move
-      expect(game.report).to eq([0, 1, 'NORTH'])
+      expect(game.report).to eq([0, 1, N])
     end
     it "passes example b" do
-      game.place(0, 0, 'NORTH')
+      game.place(0, 0, N)
       game.left
-      expect(game.report).to eq([0, 0, 'WEST'])
+      expect(game.report).to eq([0, 0, W])
     end
     it "passes example c" do
-      game.place(1, 2, 'EAST')
+      game.place(1, 2, E)
       game.move
       game.move
       game.left
       game.move
-      expect(game.report).to eq([3, 3, 'NORTH'])
+      expect(game.report).to eq([3, 3, N])
     end
   end
 end
