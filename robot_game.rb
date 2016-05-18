e906a11ec2467e68d17105b7889170c6bc0f7c08
peterlind <peter@lind.be>
2016-05-18 09:46:03 +0200

    take a more object-oriented approach

--- robot_game.rb
+++ robot_game.rb
@@ -1,7 +1,72 @@
 # frozen_string_literal: true
 class RobotGame
-  VALID_X = 0..4
-  VALID_Y = 0..4
+  def initialize
+    @placement = Placement.unplaced
+  end
+
+  def place(x, y, direction = 'NORTH')
+    new_placement = Placement.place(x, y, direction)
+    @placement = new_placement if new_placement.valid?
+  end
+
+  def move
+    @placement = Move.move(@placement)
+  end
+
+  def left
+    @placement = Placement.place(@placement.x, @placement.y, Direction.left(@placement))
+  end
+
+  def right
+    @placement = Placement.place(@placement.x, @placement.y, Direction.right(@placement))
+  end
+
+  def report
+    @placement.report
+  end
+end
+
+class Move
+  class << self
+    def move(placement)
+      new_placement =
+        case placement.direction
+        when 'EAST'
+          east_move(placement)
+        when 'WEST'
+          west_move(placement)
+        when 'NORTH'
+          north_move(placement)
+        when 'SOUTH'
+          south_move(placement)
+        end
+      new_placement.valid? ? new_placement : placement
+    end
+
+    private
+    def east_move(placement)
+      new_x = placement.x + 1
+      Placement.place(new_x, placement.y, placement.direction)
+    end
+
+    def west_move(placement)
+      new_x = placement.x - 1
+      Placement.place(new_x, placement.y, placement.direction)
+    end
+
+    def north_move(placement)
+      new_y = placement.y + 1
+      Placement.place(placement.x, new_y, placement.direction)
+    end
+
+    def south_move(placement)
+      new_y = placement.y - 1
+      Placement.place(placement.x, new_y, placement.direction)
+    end
+  end
+end
+
+class Direction
   ORDERED_VALID_DIRECTIONS = [
     'NORTH'.freeze,
     'EAST'.freeze,
@@ -9,69 +74,68 @@ class RobotGame
     'WEST'.freeze,
   ].freeze
 
-  def initialize
-    @has_placement = false
-  end
+  class << self
+    def left(placement)
+      new_index = ORDERED_VALID_DIRECTIONS.index(placement.direction) - 1
+      ORDERED_VALID_DIRECTIONS[new_index]
+    end
 
-  def place(x, y, facing = 'NORTH')
-    x, y = [x, y].map(&:to_i)
-    return unless on_board?(x, y)
-    return unless valid_direction?(facing)
-    @current_x = x
-    @current_y = y
-    @current_facing = facing
-    @has_placement = true
-  end
+    def right(placement)
+      current_index = ORDERED_VALID_DIRECTIONS.index(placement.direction)
+      ORDERED_VALID_DIRECTIONS.rotate[current_index]
+    end
 
-  def move
-    return unless @has_placement
-
-    case @current_facing
-    when 'EAST'
-      new_x = @current_x + 1
-      @current_x = new_x if on_board?(new_x, @current_y)
-    when 'WEST'
-      new_x = @current_x - 1
-      @current_x = new_x if on_board?(new_x, @current_y)
-    when 'NORTH'
-      new_y = @current_y + 1
-      @current_y = new_y if on_board?(@current_x, new_y)
-    when 'SOUTH'
-      new_y = @current_y - 1
-      @current_y = new_y if on_board?(@current_x, new_y)
+    def valid?(direction)
+      ORDERED_VALID_DIRECTIONS.include?(direction)
     end
   end
+end
 
-  def left
-    return unless @has_placement
+class Placement
+  VALID_X = 0..4
+  VALID_Y = 0..4
 
-    new_index = ORDERED_VALID_DIRECTIONS.index(@current_facing) - 1
-    new_direction = ORDERED_VALID_DIRECTIONS[new_index]
+  attr_accessor :x, :y, :direction
 
-    @current_facing = new_direction
-  end
-
-  def right
-    return unless @has_placement
-
-    current_index = ORDERED_VALID_DIRECTIONS.index(@current_facing)
-    new_direction = ORDERED_VALID_DIRECTIONS.rotate[current_index]
-
-    @current_facing = new_direction
+  def valid?
+    true
   end
 
   def report
-    return unless @has_placement
-
-    [@current_x, @current_y, @current_facing]
+    [@x, @y, @direction]
   end
 
   private
-  def valid_direction?(direction)
-    ORDERED_VALID_DIRECTIONS.include?(direction)
+  def initialize(x, y, direction)
+    @x = x
+    @y = y
+    @direction = direction
   end
 
-  def on_board?(x, y)
-    VALID_X.include?(x) && VALID_Y.include?(y)
+  class << self
+    def place(x, y, direction)
+      return NullPlacement.new unless Direction.valid?(direction)
+      return NullPlacement.new unless on_board?(x, y)
+      Placement.new(x, y, direction)
+    end
+
+    def unplaced
+      NullPlacement.new
+    end
+
+    private
+    def on_board?(x, y)
+      VALID_X.include?(x) && VALID_Y.include?(y)
+    end
+  end
+
+  class NullPlacement
+    def report
+      nil
+    end
+
+    def valid?
+      false
+    end
   end
 end
